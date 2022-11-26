0239c058e1b8786ae887d97950c1988e5fab5b82
Sergey Kozodoy <kozodoy_sv@app-sol.ru>
2022-11-26 11:20:11 +0300

    HW #5 fix ListStorage attribute type ArrayList 🠖 List

--- src/storage/ListStorage.java
+++ src/storage/ListStorage.java
@@ -3,10 +3,11 @@ package storage;
 import model.Resume;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListStorage extends AbstractStorage {
 
-    protected ArrayList<Resume> storage = new ArrayList<>();
+    protected List<Resume> storage = new ArrayList<>();
 
     @Override
     public void clear() {
