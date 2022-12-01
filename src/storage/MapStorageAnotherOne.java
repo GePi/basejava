bcf2db498262816b093e3c2ae4c98cab504bc9b1
Sergey Kozodoy <sergey.kozodoy@gmail.com>
2022-12-01 09:47:06 +0300

    HW #6 Part 2

---
+++ src/storage/MapStorageAnotherOne.java
@@ -0,0 +1,64 @@
+package storage;
+
+import model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class MapStorageAnotherOne extends AbstractStorage {
+    protected HashMap<String, Resume> storage = new HashMap<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        List<Resume> sortedStorageList = new ArrayList<>(storage.values());
+        sortedStorageList.sort(Resume.COMPARE_BY_NAME);
+        return sortedStorageList;
+    }
+
+    @Override
+    protected void doUpdate(Object searchKey, Resume r) {
+        storage.put((String) searchKey, r);
+    }
+
+    @Override
+    protected void doSave(Object searchKey, Resume r) {
+        storage.put((String) searchKey, r);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return Objects.nonNull(searchKey);
+    }
+}
--- test/storage/AllStorageTest.java
+++
@@ -1,5 +0,0 @@
-package storage;
-
-@Suite
-public class AllStorageTest {
-}
